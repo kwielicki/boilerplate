865bfbd1111e2cba18a5e88adfde177fea1c28f8
k.wielicki <krzysztofwielicki86@gmail.com>
2016-11-05 13:16:27 +0100

    Bolierplate initial

---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+var gulp			= require('gulp'),
+	browserSync 	= require('browser-sync'),
+	plumber 		= require('gulp-plumber'),
+	concat  		= require('gulp-concat'),
+	jade 			= require('gulp-jade'),
+	sass			= require('gulp-sass'),
+	autoprefixer	= require('gulp-autoprefixer'),
+	sourcemaps		= require('gulp-sourcemaps'),
+	zip 			= require('gulp-zip'),
+	sitemap 		= require('gulp-sitemap');
+
+//- Paths variables
+var devJSPath 			= "dev/javascript/",
+	devJSVendorPath 	= devJSPath + "vendor/",
+	devJADEPath			= "dev/jade/*.jade",
+	prodAssetsPath 		= "html/assets/",
+	protAssetsJSPath 	= prodAssetsPath + "javascript/";
+var devSASSPath			= "dev/sass/",
+	prodAssetsCSSPath	= prodAssetsPath + "css/";
+
+//- Concat variables
+var vendorJSConcat = "vendor.min.js";
+
+//- Static Server
+gulp.task('browser-sync', function() {
+    return browserSync.init({
+        server: {
+            baseDir: 'html'
+        },
+        ghostMode: false
+    });
+});
+
+//- Vendor frameworks / helpers / plugins object
+var vendorJSobject = {
+	framework: {
+		jquery: "jquery.min.1.11.3.js"
+	},
+	helpers: {
+		browser: "browser.min.js",
+		device: "device.min.js",
+		placeholder: "jquery.placeholder.min.js",
+		jqueryVisible: 'jquery.visible.min.js',
+		tooltip: 'tooltip.js'
+	}
+}
+
+//- Jade compiler
+gulp.task('jade', function() {
+	return gulp.src(devJADEPath)
+	.pipe(plumber())
+	.pipe(jade({
+    	pretty: true
+	}))
+	.pipe(gulp.dest('html'));
+});
+
+//- Javascript vendor
+gulp.task('js-vendor', function() {
+    return gulp.src([
+            devJSVendorPath + vendorJSobject.framework.jquery,
+            devJSVendorPath + vendorJSobject.helpers.browser,
+            devJSVendorPath + vendorJSobject.helpers.device,
+            devJSVendorPath + vendorJSobject.helpers.placeholder,
+			devJSVendorPath + vendorJSobject.helpers.jqueryVisible,
+			devJSVendorPath + vendorJSobject.helpers.tooltip
+    ])
+    .pipe(plumber())
+    .pipe(concat(vendorJSConcat))
+    .pipe(gulp.dest(protAssetsJSPath))
+});
+
+//- Javascript main
+gulp.task('js-main', function() {
+	return gulp.src(devJSPath + 'main.js')
+		.pipe(plumber())
+		.pipe(gulp.dest(protAssetsJSPath))
+});
+
+//- Javascript plugins
+gulp.task('js-plugins', function() {
+	return gulp.src(devJSPath + 'plugins/**/*.js')
+		.pipe(plumber())
+		.pipe(gulp.dest(protAssetsJSPath + 'plugins'))
+});
+
+//- Sass Bootstrap
+gulp.task('sass-bootstrap', function() {
+	return gulp.src(devSASSPath + 'bootstrap/bootstrap.scss')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('bootstrap.min.css'))
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest(prodAssetsCSSPath))
+		.pipe(browserSync.stream());
+})
+
+//- Sass Site
+gulp.task('sass-site', function() {
+	return gulp.src(devSASSPath + 'site/style.scss')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(concat('style.min.css'))
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest(prodAssetsCSSPath))
+		.pipe(browserSync.stream());
+});
+
+//- Sitemap
+gulp.task('sitemap', function() {
+	gulp.src('html/*.html', {
+		read: false
+	})
+	.pipe(sitemap({
+		siteUrl: 'http://www.netmyk.pl'
+	}))
+	.pipe(gulp.dest('html'))
+});
+
+//- Gulp copy images from DEV to PROD
+gulp.task('copy', function() {
+	gulp.src(['dev/images/*.jpg', 'dev/images/*.png', 'dev/images/**/*.png', 'dev/images/**/*.jpg'])
+		.pipe(gulp.dest('html/assets/images'))
+});
+
+//- Zip HTML {ignored by .gitignore}
+gulp.task('zip', function() {
+	return gulp.src('html/**')
+		.pipe(zip('pack.zip'))
+		.pipe(gulp.dest(''));
+})
+
+//- Gulp Watcher
+gulp.task('watch', function() {
+    gulp.watch('dev/jade/**/*.jade', ['jade']);
+    gulp.watch('dev/sass/**/*.scss', ['sass-site']);
+    gulp.watch('dev/sass/site/style.scss', ['sass-site']);
+    gulp.watch('dev/javascript/vendor/*.js', ['js-vendor']);
+		gulp.watch('dev/javascript/plugins/**/*.js', ['js-plugins']);
+    gulp.watch('dev/javascript/main.js', ['js-main']);
+    gulp.watch('html/*.html', browserSync.reload);
+    gulp.watch('html/assets/javascript/*.js', browserSync.reload);
+		gulp.watch('html/assets/javascript/**/*.js', browserSync.reload);
+});
+
+//- Gulp default
+gulp.task('default', [
+    'watch',
+    'browser-sync'
+]) ;
