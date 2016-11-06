36fc105a0a116a8d2457782fff0ddde1cc41c986
Krzysztof Wielicki <krzysztofwielicki86@gmail.com>
2016-11-06 21:14:18 +0100

    jQuery Placeholder - wywołanie funkcji na inputach, scroller, parallax

--- prod/assets/js/core.js
+++ prod/assets/js/core.js
@@ -21,17 +21,64 @@
     $(document).on('ready', function() {
 
         /** jQuery browser / device **/
-        $html.addClass('ver-' + $.browser.versionNumber);
-        $html.addClass('device_width-' + $deviceWidth);
-        $html.addClass('device_height-' +$deviceHeight);
-        if ($.browser.webkit) {
-            $html.addClass('browser-webkit');
-        } else if ($.browser.msie) {
-            $html.addClass('browser-msie');
-        } else if ($.browser.mozilla) {
-            $html.addClass('browser-mozilla');
-        }
-        
+            $html.addClass('ver-' + $.browser.versionNumber);
+            $html.addClass('device_width-' + $deviceWidth);
+            $html.addClass('device_height-' +$deviceHeight);
+            if ($.browser.webkit) {
+                $html.addClass('browser-webkit');
+            } else if ($.browser.msie) {
+                $html.addClass('browser-msie');
+            } else if ($.browser.mozilla) {
+                $html.addClass('browser-mozilla');
+            }
+
+        /** Placeholder **/
+            $('input, textarea').placeholder({
+                customClass: "js-placeholder"
+            });
+
+        /** Page scroller **/
+            if ($('[data-scroll]').length > 0) {
+                $('[data-scroll]').on('click', function(e) {
+                    var $scroll;
+                    e.preventDefault();
+                    $scroll = $(this).attr('data-scroll');
+                    if ($scroll === 'up') {
+                        $('html, body').animate({
+                            scrollTop: $($scroll).offset().top
+                        }, 900, 'swing');
+                    } else if ($scroll.charAt(0) === '#') {
+                        if ($html.hasClass('mobile')) {
+                            $('html, body').animate({
+                                scrollTop: $($scroll).offset().top
+                            }, 900, 'swing');
+                        } else {
+                            $('html, body').animate({
+                                scrollTop: $($scoll).offset().top
+                            }, 900, 'swing');
+                        }
+                    }
+                    return false;
+                });
+            }
+
+        /** Parallax Skrollr **/
+            if (!$html.hasClass('mobile') && !$html.hasClass('tablet') && $deviceWidth > 991) {
+                $('[data-parallax]').each(function() {
+                    var $this = $(this),
+                        $item = $this.attr('data-parallax');
+
+                    $this.attr('data-top-bottom', 'background-position: 50% -' + $item + 'px');
+                    $this.attr('data-bottom-top', 'background-position: 50% ' + $item + 'px');
+                    $this.attr('data-center', 'background-position: 50% 0px');
+                })
+            } else {
+                $('[data-parallax]').each(function() {
+                    var $this = $(this);
+
+                    $this.addClass('js-parallax-disabled');
+                })
+            }
 	}); //- Document on ready [end]
 
 	$(window).on('load', function() {
